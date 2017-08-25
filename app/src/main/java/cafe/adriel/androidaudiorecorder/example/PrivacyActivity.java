bbf46bd39ec758a0f205ee985d9d75eb7fce48d4
Mina Khan <minkhn01>
2017-08-25 10:00:25 -0400

    flow fixed

---
+++ app/src/main/java/cafe/adriel/androidaudiorecorder/example/PrivacyActivity.java
@@ -0,0 +1,21 @@
+package cafe.adriel.androidaudiorecorder.example;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by minakhan on 8/24/17.
+ */
+
+public class PrivacyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy);
+
+    }
+}
